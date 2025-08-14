61c82d54a54eacc4d09d55e76c04de7068b3af18
Carley Kuehner <43360628+starleycarley@users.noreply.github.com>
2025-08-14 12:46:05 -0400

    Refine guarantee section layout

--- front-page.php
+++ front-page.php
@@ -156,7 +156,7 @@ document.addEventListener( 'DOMContentLoaded', function() {
                     <a href="/breeders/" class="btn btn-primary mt-3"><?php esc_html_e( 'Learn More', 'happiness-is-pets' ); ?></a>
                 </div>
                 <div class="col-md-6 text-center">
-                    <img src="/media/background/caninecare.webp" alt="<?php esc_attr_e( 'Canine Care Certified', 'happiness-is-pets' ); ?>" class="img-fluid rounded" />
+                    <img src="https://www.happinessispets.com/media/background/caninecare.webp" alt="<?php esc_attr_e( 'Canine Care Certified', 'happiness-is-pets' ); ?>" class="img-fluid rounded" />
                 </div>
             </div>
         </div>
@@ -170,7 +170,7 @@ document.addEventListener( 'DOMContentLoaded', function() {
                         <div class="row">
                             <div class="col-12 col-md-6 col-lg-4 order-1 order-md-1 d-flex align-items-center">
                                 <div class="info-layout-content-img">
-                                    <img src="/media/background/ourpuppies.webp" alt="" class="info_placeholder img-fluid infofirst-img" />
+                                    <img src="https://www.happinessispets.com/media/background/ourpuppies.webp" alt="" class="info_placeholder img-fluid infofirst-img" />
                                 </div>
                             </div>
                             <div class="col-12 col-md-6 col-lg-8 align-items-center d-flex order-2 order-md-2">
@@ -204,6 +204,36 @@ document.addEventListener( 'DOMContentLoaded', function() {
         <!-- half image and half text and button area -->
 
     </div>
+
+<?php // --- Our Guarantee Section --- ?>
+    <section class="front-page-section py-5 my-5" id="our-guarantee">
+        <div class="container">
+            <h2 class="text-center"><?php esc_html_e( 'Our Guarantee', 'happiness-is-pets' ); ?></h2>
+            <p class="text-center"><span style="font-size: 20px; "><?php esc_html_e( 'Peace of mind for you and your puppy', 'happiness-is-pets' ); ?></span></p>
+            <div class="row justify-content-center">
+                <div class="col col-12 col-md-4 align-self-center">
+                    <div class="container d-flex flex-column justify-content-center align-items-center">
+                        <img src="https://www.happinessispets.com/media/filer_public_thumbnails/filer_public/dc/cf/dccf0cd9-751e-47b4-b42c-8000445d9d9c/health-warranty.webp__88.0x77.0_subsampling-2.webp" alt="" class="img-fluid" />
+                        <div class="my-2 text-center">
+                            <p class="primary1-bold"><?php esc_html_e( '2 year health warranty', 'happiness-is-pets' ); ?></p>
+                            <p><?php esc_html_e( 'Since many, but not all congenital defects arise within the first year, we go the extra mile to ensure you and your new puppy are covered.', 'happiness-is-pets' ); ?></p>
+                        </div>
+                        <a href="/health-warranty/" class="theme-primary-btn" style="display: block; width: fit-content; "><?php esc_html_e( 'Health Warranty', 'happiness-is-pets' ); ?></a>
+                    </div>
+                </div>
+
+                <div class="col col-12 col-md-4 align-self-center my-5 my-md-0">
+                    <div class="container d-flex flex-column justify-content-center align-items-center">
+                        <img src="https://www.happinessispets.com/media/filer_public_thumbnails/filer_public/58/24/582480e6-b190-420b-83bb-58f9812c876c/veterinary-check.webp__81.0x68.0_subsampling-2.webp" alt="" class="img-fluid" />
+                        <div class="my-2 text-center">
+                            <p class="primary1-bold"><?php esc_html_e( '7 day veterinary check', 'happiness-is-pets' ); ?></p>
+                            <p><?php esc_html_e( 'Bring your new puppy to any of our in-network clinics within 7 days of purchase for a complimentary wellness check-up.', 'happiness-is-pets' ); ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 <?php // --- Reviews Section --- ?>
     <section class="front-page-section py-5" id="reviews">
